eb30c39de369d2bdd12b7ae0e9a0ff346322e288
민경 <149250433+kyoooooong@users.noreply.github.com>
2026-02-13 03:50:42 +0900

    [level 0] Title: rny_string, Time: 0.05 ms, Memory: 93.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rny_string) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < rny_string.length(); i++) {
+            char ch = rny_string.charAt(i);
+            if (ch == 'm') {
+                sb.append("rn");
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
